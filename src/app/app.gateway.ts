7eb808bd9541c3b458d9312b04e42651f6cfc0b8
PavlovichVladislav <jeffry2814@gmail.com>
2025-05-10 13:50:51 +0700

    prisma & gateway

---
+++ src/app/app.gateway.ts
@@ -0,0 +1,41 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { AppService } from 'src/app.service';
+import { Server, Socket } from 'socket.io';
+import { Prisma } from '@prisma/client';
+
+@WebSocketGateway()
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(private appService: AppService) {}
+
+  @WebSocketServer() server: Server;
+
+  @SubscribeMessage('sendMessage')
+  async handleMessage(
+    client: Socket,
+    payload: Prisma.ChatCreateInput,
+  ): Promise<void> {
+    await this.appService.createMessage(payload);
+    this.server.emit('recMessage', payload);
+  }
+
+  afterInit(server: any) {
+    console.log(server);
+  }
+
+  handleConnection(client: Socket) {
+    console.log('connected', client.id);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('disconnected', client.id);
+  }
+}
